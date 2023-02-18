46a235a9155ee91c25a833fbf7a9666417066b7e
kateFrontend <ecdanilova@gmail.com>
2023-02-18 14:19:46 -0500

    select random tag

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
+
+  if(e.key === 'Enter') {
+    setTimeout(() => {
+        e.target.value = ''
+    }, 10)
+
+    randomSelect()
+  }
 });
 
 function createTags(input) {
@@ -23,4 +31,51 @@ function createTags(input) {
         tagEl.innerText = tag
         tagsEl.appendChild(tagEl)
     })
+}
+
+// Random Select functionality
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+
+        // shift it to each one, highlight and highlight after a certain amount of time
+
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+    }, 100);
+
+        // stop it and pick a random tag to land on and highlight
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
+}
+
+// Select Random Tag
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
 }
